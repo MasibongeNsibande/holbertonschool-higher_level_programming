37c4a8bf2129089ca659e5605c4f5bbe332ff859
MasibongeNsibande <nsibandemasibonge@gmail.com>
2023-05-31 06:20:42 -0700

    tasks

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j))
-        else:
-            print("{:d}{:d}, ".format(i, j), end="")
-            
+
+for i in range(100):
+    if i < 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:d}".format(i))
+        
